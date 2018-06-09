f23a52904a6eef0da7938c09ec9280d79b6e216d
imnotbob <Imnot_bob@hotmail.com>
2018-06-09 10:15:29 -0400

    Update orbit-valve.groovy

--- devicehandlers/imnotbob/orbit-valve.src/orbit-valve.groovy
+++ devicehandlers/imnotbob/orbit-valve.src/orbit-valve.groovy
@@ -23,6 +23,7 @@
 
 metadata {
 	definition (name: "Orbit Valve", namespace: "E_Sch", author: "Eric, Gene Ussery, others") {
+		capability "Actuator"
 		capability "Battery"
 		capability "Configuration"
 		capability "Refresh"
